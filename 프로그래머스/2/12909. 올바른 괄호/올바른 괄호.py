616b68ead5f291c023f7496ae2b4be88d8abf37d
Kang NaYeon <125345160+dusskdus@users.noreply.github.com>
2025-08-13 22:01:34 +0900

    [level 2] Title: 올바른 괄호, Time: 7.79 ms, Memory: 9.38 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,15 @@
+def solution(s):
+    answer = True
+    stack = []
+    for i in s:
+        if i =="(":
+            stack.append(i)
+        elif i ==")":
+            if stack == []:
+                answer = False
+            else:
+                stack.pop()
+    if stack != []:
+        answer = False
+        
+    return answer
